1b440d428ec11af77d6f9a2300b6319604e104dd
altananay <altananay@outlook.com>
2021-12-22 21:13:58 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager:BaseCustomerManager, ICustomerCheckService
+    {
+        private ICustomerCheckService _customerCheckService;
+
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void Save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı vatandaş bilgisi.");
+            }
+            
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            //customerManager.Save(new Customer { DateOfBirth = new DateTime(2000, 4,10), FirstName="Suzan", LastName = "YILMAZ",NationalityId="10877687846" });
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(2000, 6, 2), FirstName = "Cumhur Utku", LastName = "ATAK", NationalityId = "15838659016" });
+        }
+    }
+}
